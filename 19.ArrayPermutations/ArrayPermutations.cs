303d17c127d2ded300f4329041f7597d4b629f5a
ms-nenova <ms.nenova@gmail.com>
2013-02-16 23:47:51 +0200

    CSharp

---
+++ 19.ArrayPermutations/ArrayPermutations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ArrayPermutations
+{
+    static void SwapValues<T>(T[] arr, int startPos, int endPos)            //Here we will swap the values of the array.
+    {
+        if (startPos != endPos)
+        {
+            T temporary = arr[startPos];                                    //We need this to keep the value from the starting position.
+            arr[startPos] = arr[endPos];
+            arr[endPos] = temporary;
+        }
+    }
+    static IEnumerable<T[]> Permutations<T>(T[] arr, int startIndex = 0)    //Here we will calculate the permutations with an iterator method.
+    {
+        if (startIndex + 1 == arr.Length)
+        {
+            yield return arr;                                              //We use a yield return statement to return each element one at a time.
+        }
+        else
+        {
+            foreach (var item in Permutations(arr, startIndex + 1))        //We go to the next value.
+            {
+                yield return item;
+            }
+            for (var i = startIndex + 1; i < arr.Length; i++)              //In the loop we use the swapping method to change position of the values.
+            {
+                SwapValues(arr, startIndex, i);
+                foreach (var item in Permutations(arr, startIndex + 1))
+                {
+                    yield return item;
+                }
+                SwapValues(arr, startIndex, i);
+            }
+        }
+    }
+    static void Main()
+    {
+        Console.Write("Enter a number N to make permutations:");
+        int N = int.Parse(Console.ReadLine());
+        int[] arrayOfNumbers = new int[N];
+        for (int i = 1; i <= N; i++)
+        {
+            arrayOfNumbers[i - 1] = i;
+        }
+
+        foreach (var item in Permutations(arrayOfNumbers))
+        {
+            Console.WriteLine(string.Join(", ", item));
+        }
+    }
+}
---
+++ 8.ShortToBinary/ShortToBinary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*I use the same logic as in DecimalToBinary, only the bit's representation is for short*/
+class ShortToBinary
+{
+    static void Main()
+    {
+        Console.Write("Enter a number of type short (from -32768 to 32767): ");
+        short number = short.Parse(Console.ReadLine());
+        Console.WriteLine(DecToBin(number));
+    }
+    static string DecToBin(short number) //this algorithm is based on the size of an integer, which has 15 bits(including 0)
+    {
+        char[] bit = new char[16];
+        short pos = 15;
+        short i = 0;
+
+        while (i < 16)
+        {
+            if ((number & (1 << i)) != 0) //go to each position and check with this bitwise operation if the value is 0 or 1
+            {
+                bit[pos] = '1';
+            }
+            else
+            {
+                bit[pos] = '0';
+            }
+            pos--;
+            i++;
+        }
+        return new string(bit);
+    }
+}
