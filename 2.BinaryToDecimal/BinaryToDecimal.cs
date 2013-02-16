b688d95d3bc2a957ee304f0ce6aa411d829e2ac6
ms-nenova <ms.nenova@gmail.com>
2013-02-16 22:46:01 +0200

    Numeral Systems

---
+++ 2.BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        Console.Write("Enter a binary number:");
+        string binary = Convert.ToString(Console.ReadLine()); 
+        int decimalNumber = 0;
+        for (int i = 0; i < binary.Length; i++)
+        {
+            if (binary[binary.Length - i - 1] == '0')    //we get the bits from right to the left; 
+            {                                            //if the number has '1' on position 31 (counting first position = 0), the decimal representation will be negative
+                continue;
+            }
+            decimalNumber += (int)Math.Pow(2, i);        //we calculate the decimal number by the formula the bit*2^i
+        }
+        Console.WriteLine("The decimal representation of {0} is: {1}",binary,decimalNumber);
+    }
+}
